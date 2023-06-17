47922888db72c361c24aa874b27b9573db632382
Tom Poyarkov <poart1405@gmail.com>
2023-06-17 22:19:04 +0300

    extend request class

--- app/Routing/Request.php
+++ app/Routing/Request.php
@@ -4,14 +4,27 @@ namespace App\Routing;
 
 class Request
 {
+    protected string $url = "";
+    protected array $query = [];
+
+    protected array $params = [];
+
     public function __construct(
         protected string $host,
         protected string $method,
-        protected string $url
+        protected string $uri
     )
-    {}
+    {
+        $this->url = explode("?", $uri)[0];
+
+        // Recommended
+        $query = [];
+        parse_str($_SERVER["QUERY_STRING"], $query);
+        $this->query = $query;
+    }
 
     public static function get(): Request {
+
         return new Request($_SERVER["HTTP_HOST"], $_SERVER['REQUEST_METHOD'], $_SERVER['REQUEST_URI']);
     }
 
@@ -37,5 +50,33 @@ class Request
     public function getUrl(): string
     {
         return $this->url;
+    }
+
+    /**
+     * @param array $params
+     * @return Request
+     */
+    public function setParams(array $params): Request
+    {
+        $this->params = $params;
+        return $this;
+    }
+
+
+    /**
+     * @param array $params
+     * @return Request
+     */
+    public function getParam(string $key): ?string
+    {
+        return $this->params[$key] ?? null;
+    }
+
+    /**
+     * @return array
+     */
+    public function getQuery(string $key): mixed
+    {
+        return $this->query[$key] ?? null;
     }
 }
